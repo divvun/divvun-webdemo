0b08adc3045be3c5c5cede4c52104c0b85735db5
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2015-12-17 14:36:20 +0100

    replacement works

--- app.js
+++ app.js
@@ -61,8 +61,8 @@
     // Ensure the first error (by start-offset) is first:
     errors.sort(function(a,b){return a[0] - b[0];});
 
-    var form = document.getElementById('form');
-    $('#form').empty();
+    var form = $('#form');
+    form.empty();
     for(var i=0, done=0; i < errors.length; i++)
     {
       var beg = errors[i][0],
@@ -71,35 +71,36 @@
           rep = errors[i][3],
           pre = text.slice(done, beg),
           err = text.slice(beg, end),
-          span = document.createElement('span');
+          span = $(document.createElement('span'));
       if(beg < done) {
         console.log("Overlapping (or unsorted) errors! Skipping error "+errors[i]);
         continue;
       }
-      console.log(done,beg,end,typ,pre,"←pre,err→",err);
-      form.appendChild(document.createTextNode(pre));
-      span.textContent = err;
-      $(span).click({typ:typ, rep:rep},
+      // console.log("!",done,beg,end,typ,pre,"←pre,err→",err);
+      form.append($(document.createTextNode(pre)));
+      span.text(err);
+      span.click({typ:typ, rep:rep},
                     function (e) {
                       e.stopPropagation();
-                      return showsugg(this, e.data.typ, e.data.rep);
+                      return showrep(this, e.data.typ, e.data.rep);
                     });
-      span.className += " error "+typ;
-      form.appendChild(span);
+      span.addClass("error");
+      form.append(span);
       done = end;
     }
-    form.appendChild(document.createTextNode(text.slice(done)));
+    form.append($(document.createTextNode(text.slice(done))));
   };
 
   /**
    * Changes DOM
    */
-  var hidesugg = function() {
-    //console.log("hidesugg");
-    var suggmenu = $('#suggmenu');
-    suggmenu.offset({top:0, left:0}); // avoid some potential bugs with misplacement
-    suggmenu.hide();
+  var hiderep = function() {
+    //console.log("hiderep");
+    var repmenu = $('#repmenu');
+    repmenu.offset({top:0, left:0}); // avoid some potential bugs with misplacement
+    repmenu.hide();
   };
+
   /**
    * Changes DOM
    * TODO: populate menu, handle replacement
@@ -108,30 +109,79 @@
    * @param {string} typ
    * @param {Array} rep
    */
-  var showsugg = function(span, typ, rep) {
-    //console.log("showsugg");
+  var showrep = function(span, typ, rep) {
+    //console.log("showrep");
     var spanoff = $(span).offset();
     var newoff = { top:  spanoff.top+20,
                    left: spanoff.left };
-    var suggmenu = $('#suggmenu');
-    var at_same_err = suggmenu.offset().top == newoff.top && suggmenu.offset().left == newoff.left;
-    if(suggmenu.is(":visible") && at_same_err) {
-      hidesugg();
+    var repmenu = $('#repmenu');
+    var at_same_err = repmenu.offset().top == newoff.top && repmenu.offset().left == newoff.left;
+    if(repmenu.is(":visible") && at_same_err) {
+      hiderep();
     }
     else {
-      suggmenu.show();
-      suggmenu.offset(newoff);
-      console.log(typ);
+      repmenu.show();
+      repmenu.offset(newoff);
       if(!at_same_err) {
-        $("#suggmenu_tbl").html(typ);
+        makerepmenu(span, typ, rep);
       }
     }
   };
 
+  /**
+   * Changes DOM
+   * Populates menu.
+   * TODO: ignore-button
+   *
+   * @param {Node} span
+   * @param {string} typ
+   * @param {Array} rep
+   */
+  var makerepmenu = function(span, typ, rep) {
+    // We're looking at a new error, populate the table anew:
+    $("#repmenu_tbl").empty();
+    var tbody = $(document.createElement('tbody')),
+        tr_title =  $(document.createElement('tr')),
+        td_title =  $(document.createElement('td')),
+        a_title =  $(document.createElement('a'));
+    a_title.text(typ);
+    a_title.attr("aria-disabled", "true");
+    td_title.append(a_title);
+    td_title.addClass("repmenu_title");
+    tr_title.append(td_title);
+    tbody.append(tr_title);
+
+    rep.map(function(r){
+      var tr_rep =  $(document.createElement('tr')),
+          td_rep =  $(document.createElement('td')),
+          a_rep =  $(document.createElement('a'));
+      a_rep.text(r);
+      td_rep.append(a_rep);
+      td_rep.addClass("repmenu_rep");
+      td_rep.addClass("repmenu_nonfirst");
+      // has to be on td since <a> doesn't fill the whole td
+      td_rep.click({span:span, r:r},
+                   function(e){
+                     var r = e.data.r,
+                         span = $(e.data.span);
+                     span.replaceWith(r);
+                     hiderep();
+                   });
+      tr_rep.append(td_rep);
+      tbody.append(tr_rep);
+    });
+
+    $("#repmenu_tbl").append(tbody);
+  };
+
   var init = function () {
     $("#check_b").click(checkit);
-    $("#form").click(hidesugg);
+    $("#form").click(hiderep);
+
+    // DEBUG:
     checkit();
+    $('.error')[0].click();
+
   };
   window.onload=init;
 
