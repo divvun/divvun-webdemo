df13649615a1c73cad271fbd4cea820a5297f146
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2016-11-17 21:03:59 +0100

    check right after login

--- src/app.js
+++ src/app.js
@@ -445,6 +445,7 @@ var loginFromForm = function() {
   var userpass = readLoginFormStoring();
   if(userpass != null) {
     hideLogin();
+    check();
   }
   // Ensure we don't reload the page (due to input type submit):
   return false;
