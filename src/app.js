a0d46b8f080fbe8330b37218d07f8487bac53d02
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2018-09-10 16:44:14 +0200

    wip

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@
 /* :: type cb = (text: string, X:result, off: number) => void */
 /* :: type authcb = (text: string) => void */
 /* :: type userpass = {u: string, p: string}|null */
-/* :: type mode = { src: string, trglang: string, trgsuff: string } */
+/* :: type mode = { specLanguage: string, specLanguage3: string, pipeLanguage: string, pipeLanguage3: string, pipename: string } */
 
 var debug = window.location.protocol === "file:";
 var log = debug ? console.log.bind(window.console) : function() {};
@@ -391,8 +391,8 @@ if(hostname === "localhost") {
 
 // TODO: apy should have an endpoint for grammar checkers, and expect its modes files in a separate dir!
 // (endpoint both for listing and "translate")
-var modesUrl/*:string*/ = protocol+"//"+hostname+":"+(port.toString())+subdir+"/listPairs";
-var checkUrl/*:string*/ = protocol+"//"+hostname+":"+(port.toString())+subdir+"/translateRaw";
+var modesUrl/*:string*/ = protocol+"//"+hostname+":"+(port.toString())+subdir+"/list?q=checkers";
+var checkUrl/*:string*/ = protocol+"//"+hostname+":"+(port.toString())+subdir+"/checker";
 log(checkUrl);
 var hunUrl/*:string*/ = protocol+"//"+hostname+":"+(port.toString())+subdir+"/hunspell";
 
@@ -463,7 +463,8 @@ var servercheck = function(userpass/*:userpass*/,
   // sent.
   let url = checkUrl;
   let data = {
-      langpair: mode,
+      lang: mode.specLanguage,
+      pipename: mode.pipename,
       q: text
   };
   if(getVariant(searchToObject()) === "hunspell") {
@@ -547,21 +548,13 @@ var getModes = function()/*: void*/ {
     type: "GET",
     data: {},
     success: function(res){
-      let modelist/*:Array<mode>*/ = res.responseData.map(function(m) {
-        let src = m.sourceLanguage;
-        let trg = m.targetLanguage;
-        let trgsuff = trg.replace(/^[^_]*_/, "");
-        let trglang = trg.replace(/_.*/, "");
-        return { src: src, trglang: trglang, trgsuff: trgsuff };
-      }).filter(function(mm) {
-        return mm.src == mm.trglang && mm.trgsuff.match(/(gram|spell)/);
-      });
+      let modelist/*:Array<mode>*/ = res.responseData;
       // skewer.log(modes);
-      Array.from(groupBy(modelist, (m) => { return m["src"]; }).entries()).map(function([k, elts]){
+      Array.from(groupBy(modelist, (m) => { return m["specLanguage"]; }).entries()).map(function([k, elts]){
         modesByLanguage[k] = elts;
         elts.forEach(modeToDropdown);
         elts.forEach(function(m) {
-          allModes.add(langToMode(m.src, m.trgsuff));
+          allModes.add(m);
         });
       });
       let search = searchToObject(),
@@ -571,12 +564,13 @@ var getModes = function()/*: void*/ {
     },
     dataType: "json"
   });
-  modeToDropdown({ src: "se_NO", trglang: "se_NO", trgsuff: "hunspell" });
+  // TODO: hunspell
+  // modeToDropdown({ src: "se_NO", trglang: "se_NO", trgsuff: "hunspell" });
 };
 
 var modeToDropdown = function(m/*:mode*/)/*:void*/ {
-  let lang = m.src,
-      variant = m.trgsuff;
+  let lang = m.specLanguage,
+      variant = m.pipename;
   let a =
       $('<a>')
       .text(lang + " " + variant)
